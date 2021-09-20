05d3db29f18479c448fa90ebe52a7e90b70e88e4
Sorin <dsorin95@gmail.com>
2021-09-20 21:56:00 +0200

    Added a view pager.

--- app/src/main/java/com/lesorin/firespark/StartActivity.java
+++ app/src/main/java/com/lesorin/firespark/StartActivity.java
@@ -1,14 +1,17 @@
 package com.lesorin.firespark;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class StartActivity extends AppCompatActivity {
+public class StartActivity extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+
     }
 }
