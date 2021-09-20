68d689902350901a4354cd3ef4da4ce5ceb3955b
Sorin <dsorin95@gmail.com>
2021-09-20 22:21:00 +0200

    Added a welcome fragment, and initialized the main view pager with it.

--- app/src/main/java/com/lesorin/firespark/view/activities/StartActivity.java
+++ app/src/main/java/com/lesorin/firespark/view/activities/StartActivity.java
@@ -1,18 +1,30 @@
 package com.lesorin.firespark.view.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 import android.os.Bundle;
 import com.lesorin.firespark.R;
+import com.lesorin.firespark.view.fragments.ViewPagerAdapter;
+import com.lesorin.firespark.view.fragments.WelcomeFragment;
 
 public class StartActivity extends AppCompatActivity
 {
-
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
+        initializeViewPager();
+    }
 
+    private void initializeViewPager()
+    {
+        ViewPager vp = findViewById(R.id.ViewPager);
+        ViewPagerAdapter vpa = new ViewPagerAdapter(getSupportFragmentManager());
+
+        vpa.addFragment(new WelcomeFragment(), "Welcome");
+
+        vp.setAdapter(vpa);
     }
 }
