64ced68398e89df461ef9c7d0d77250d8619e235
Sorin <dsorin95@gmail.com>
2021-09-20 22:25:43 +0200

    Added a login and register fragment.

--- app/src/main/java/com/lesorin/firespark/view/activities/StartActivity.java
+++ app/src/main/java/com/lesorin/firespark/view/activities/StartActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
 import android.os.Bundle;
 import com.lesorin.firespark.R;
+import com.lesorin.firespark.view.fragments.LoginFragment;
+import com.lesorin.firespark.view.fragments.RegisterFragment;
 import com.lesorin.firespark.view.fragments.ViewPagerAdapter;
 import com.lesorin.firespark.view.fragments.WelcomeFragment;
 
@@ -23,7 +25,9 @@ public class StartActivity extends AppCompatActivity
         ViewPager vp = findViewById(R.id.ViewPager);
         ViewPagerAdapter vpa = new ViewPagerAdapter(getSupportFragmentManager());
 
-        vpa.addFragment(new WelcomeFragment(), "Welcome");
+        vpa.addFragment(new WelcomeFragment(), null);
+        vpa.addFragment(new LoginFragment(), null);
+        vpa.addFragment(new RegisterFragment(), null);
 
         vp.setAdapter(vpa);
     }
---
+++ app/src/main/java/com/lesorin/firespark/view/fragments/RegisterFragment.java
@@ -0,0 +1,33 @@
+package com.lesorin.firespark.view.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import com.lesorin.firespark.R;
+
+public class RegisterFragment extends Fragment
+{
+
+    private View _view;
+
+    public RegisterFragment()
+    {
+        _view = null;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
+    {
+        if(_view == null)
+        {
+            _view = inflater.inflate(R.layout.fragment_login, container, false);
+        }
+
+        return _view;
+    }
+}
