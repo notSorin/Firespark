db56fff0ddec8cdf0c28bf654c8432f6cfb45179
Sorin <dsorin95@gmail.com>
2021-10-01 22:59:06 +0200

    Initialized navigation view.

--- app/src/main/java/com/lesorin/firespark/view/activities/MainActivity.java
+++ app/src/main/java/com/lesorin/firespark/view/activities/MainActivity.java
@@ -2,14 +2,26 @@ package com.lesorin.firespark.view.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.lesorin.firespark.R;
 
 public class MainActivity extends AppCompatActivity
 {
+    private BottomNavigationView _navigationView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initializeNavigationView();
+    }
+
+    private void initializeNavigationView()
+    {
+        _navigationView = findViewById(R.id.NavigationView);
+
+        _navigationView.setSelectedItemId(R.id.HomePage);
     }
 }
