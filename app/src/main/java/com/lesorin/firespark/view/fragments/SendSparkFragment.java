d70bc00eb92dcbbe39a933d6d3f5ec07d23a0375
Sorin <dsorin95@gmail.com>
2021-10-04 00:26:00 +0200

    Made it possible to write data for a new spark, and move it around the screen.

--- app/src/main/java/com/lesorin/firespark/view/fragments/SendSparkFragment.java
+++ app/src/main/java/com/lesorin/firespark/view/fragments/SendSparkFragment.java
@@ -1,17 +1,24 @@
 package com.lesorin.firespark.view.fragments;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import com.lesorin.firespark.R;
 
 public class SendSparkFragment extends Fragment
 {
     private View _view;
+    private LinearLayout _upToSend;
+    private ConstraintLayout _sparkLayout;
+    private float _touchX, _touchY, _differenceX, _differenceY;
 
     public SendSparkFragment()
     {
@@ -25,8 +32,56 @@ public class SendSparkFragment extends Fragment
         if(_view == null)
         {
             _view = inflater.inflate(R.layout.fragment_send_spark, container, false);
+
+            initializeSparkLayout();
+            initializeUpToSend();
         }
 
         return _view;
+    }
+
+    private void initializeSparkLayout()
+    {
+        _sparkLayout = _view.findViewById(R.id.SparkLayout);
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    private void initializeUpToSend()
+    {
+        _upToSend = _view.findViewById(R.id.UpToSend);
+
+        _upToSend.setOnTouchListener((view, event) ->
+        {
+            if(event.getAction() == MotionEvent.ACTION_DOWN)
+            {
+                _touchX = _sparkLayout.getX();
+                _touchY = _sparkLayout.getY();
+            }
+
+            if(event.getAction() == MotionEvent.ACTION_DOWN || event.getAction() == MotionEvent.ACTION_MOVE)
+            {
+                final View parent = (View)_sparkLayout.getParent();
+
+                //Action down initializes some data.
+                if(event.getAction() == MotionEvent.ACTION_DOWN)
+                {
+                    _differenceX = event.getX();
+                    _differenceY = event.getY();
+                }
+
+                //Action move moves the view around.
+                if(event.getAction() == MotionEvent.ACTION_MOVE)
+                {
+                    float y = _sparkLayout.getY() + (event.getY() - _differenceY);
+
+                    if(y > _view.getY() - 100 && y < _view.getY() + _view.getHeight() - 300)
+                    {
+                        _sparkLayout.setY(y);
+                    }
+                }
+            }
+
+            return true;
+        });
     }
 }
