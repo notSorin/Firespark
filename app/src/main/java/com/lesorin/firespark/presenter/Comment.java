6c758c24ea8ce8fd946775be58b919d1f6b997e2
Sorin <dsorin95@gmail.com>
2021-10-06 22:06:49 +0200

    Added fields to Comment.

--- app/src/main/java/com/lesorin/firespark/presenter/Comment.java
+++ app/src/main/java/com/lesorin/firespark/presenter/Comment.java
@@ -1,6 +1,37 @@
 package com.lesorin.firespark.presenter;
 
+import com.google.firebase.Timestamp;
+import com.google.firebase.firestore.PropertyName;
+import java.util.ArrayList;
+
 public class Comment
 {
-    public String _text, _ownerId, _ownerName;
+    private String _id;
+    private boolean _ownedByCurrentUser;
+
+    @PropertyName("ownerid")
+    private String _ownerId;
+
+    @PropertyName("ownerfirstlastname")
+    private String _ownerFirstLastName;
+
+    @PropertyName("ownerusername")
+    private String _ownerUsername;
+
+    @PropertyName("body")
+    private String _body;
+
+    @PropertyName("created")
+    private Timestamp _created;
+
+    @PropertyName("isdeleted")
+    private Timestamp _isDeleted;
+
+    @PropertyName("likes")
+    private ArrayList<String> _likes;
+
+    public Comment()
+    {
+        _likes = new ArrayList<>();
+    }
 }
