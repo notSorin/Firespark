24f46b18934ded104265bd035795799851310ed8
Sorin <dsorin95@gmail.com>
2021-10-11 20:14:00 +0200

    Fixed wrong visibility for logout button.

--- app/src/main/java/com/lesorin/firespark/view/fragments/ProfileFragment.java
+++ app/src/main/java/com/lesorin/firespark/view/fragments/ProfileFragment.java
@@ -81,7 +81,7 @@ public class ProfileFragment extends FragmentWithSparks
                 _userJoined.setText(String.format(getResources().getString(R.string.UserJoined),
                         _dateFormat.format(_user.getJoined().toDate())));
                 _followButton.setVisibility(_user.isCurrentUser() ? View.GONE : View.VISIBLE);
-                _logoutButton.setVisibility(_user.isCurrentUser() ? View.VISIBLE : View.INVISIBLE);
+                _logoutButton.setVisibility(_user.isCurrentUser() ? View.VISIBLE : View.GONE);
             }
         }
     }
