e3d9b421a844b7d31371e56d7c1580feecd693cf
Sorin <dsorin95@gmail.com>
2021-10-16 21:10:57 +0200

    Added an adapter class to extend FiresparkFragment with default method implementations.

---
+++ app/src/main/java/com/lesorin/firespark/view/fragments/FiresparkFragmentAdapter.java
@@ -0,0 +1,72 @@
+package com.lesorin.firespark.view.fragments;
+
+import com.lesorin.firespark.presenter.pojo.Spark;
+import com.lesorin.firespark.presenter.pojo.User;
+
+public class FiresparkFragmentAdapter extends FiresparkFragment
+{
+    @Override
+    public User getUser()
+    {
+        return null;
+    }
+
+    @Override
+    public boolean isProfileFragment()
+    {
+        return false;
+    }
+
+    @Override
+    public boolean isHomeFragment()
+    {
+        return false;
+    }
+
+    @Override
+    public boolean isPopularFragment()
+    {
+        return false;
+    }
+
+    @Override
+    public void addSpark(Spark spark)
+    {
+    }
+
+    @Override
+    public void deleteSpark(Spark spark)
+    {
+    }
+
+    @Override
+    public boolean isMainFragment()
+    {
+        return false;
+    }
+
+    @Override
+    public void sparkLiked(Spark spark)
+    {
+    }
+
+    @Override
+    public void sparkLikeRemoved(Spark spark)
+    {
+    }
+
+    @Override
+    public void setUser(User user)
+    {
+    }
+
+    @Override
+    public void userFollowed(User user)
+    {
+    }
+
+    @Override
+    public void userUnfollowed(User user)
+    {
+    }
+}
